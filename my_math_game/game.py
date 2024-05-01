63c764c3b9d206b355c55da50cd85dbebf61119f
juankgomezz <168669509+juankgomezz@users.noreply.github.com>
2024-05-01 15:49:45 -0500

    game.py

--- my_math_game/game.py
+++ my_math_game/game.py
@@ -1,21 +1,21 @@
 # Operaciones aritméticas básicas
 
-def add(num_1, num_2):
+def suma(num_1, num_2):
     return num_1 + num_2
 
-def subtract(num_1, num_2):
+def resta(num_1, num_2):
     return num_1 - num_2
 
-def multiply(num_1, num_2):
+def multiplicacion(num_1, num_2):
     return num_1 * num_2
 
-def divide(num_1, num_2):
+def division(num_1, num_2):
     if num_2 != 0:
         return num_1 / num_2
     else:
         return None
 
-def power(num_1, num_2):
+def potenciacion(num_1, num_2):
     return num_1 ** num_2
 
 def modulo(num_1, num_2):
@@ -25,36 +25,36 @@ def modulo(num_1, num_2):
 
 def game():
     score = 0
-    operations = [add, subtract, multiply, divide, power, modulo]
-    operation_names = ["Addition", "Subtraction", "Multiplication", "Division", "Power", "Modulo"]
+    operations = [suma, resta, multiplicacion, division, potenciacion, modulo]
+    operation_names = ["suma", "resta", "Multiplicacion", "Division", "potenciacion", "Modulo"]
     points = [1, 1, 2, 2, 4, 4]
 
     while True:
         print("======== Menu ========")
         print("\n".join(f"{i+1}. {name}" for i, name in enumerate(operation_names)))
-        print("0. Exit")
-        option = int(input("\nChoose an option: "))
+        print("0. salir")
+        option = int(input("\nElige una opcion: "))
 
         if option == 0:
             break
 
         if 1 <= option <= 6:
-            num_1 = int(input("Enter first number: "))
-            num_2 = int(input("Enter second number: "))
+            num_1 = int(input("ingresa el primer numero: "))
+            num_2 = int(input("ingrese el segundo numero: "))
             correct_answer = operations[option-1](num_1, num_2)
-            user_answer = float(input("What is your answer? "))
+            user_answer = float(input("cual es tu respuesta? "))
 
             if user_answer == correct_answer:
                 score += points[option-1]
-                print("Correct!")
+                print("Correcto!")
             else:
-                print("Incorrect. The correct answer is:", correct_answer)
+                print("incorrecto. la respuesta correcta es:", correct_answer)
         else:
-            print("Invalid option. Please choose a valid operation.")
+            print("opcion invalida. Por favor elige una opcion valida.")
 
-        print(f"Your current score is: {score}")
+        print(f"Tu puntuacion actual es: {score}")
 
-    print(f"======== Game Over ========\nYour final score is {score}\nKeep going!")
+    print(f"======== Game Over ========\nTu puntaje final es {score}\nsigue intentando!")
 
 # Llamar al juego para iniciar
 if __name__ == "__main__":
